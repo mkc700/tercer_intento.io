a56bc1199456f88bfa2a1c150cb01018742df35e
mkc700 <122412894+mkc700@users.noreply.github.com>
2023-04-06 09:23:20 -0600

    initial commit

---
+++ ejemplo.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <a href="" class="text-decoration-none">
+        <div class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg"  fill="#E07078" height="35" viewBox="0 96 960 960" width="35"><path d="M222.152 941.978q-28.104 0-48.117-20.013-20.013-20.013-20.013-48.117V486.957q0-16.137 7.212-30.574 7.212-14.437 20.136-23.883l257.608-193.326q9.491-6.718 19.874-10.196 10.384-3.478 21.364-3.478t21.209 3.478q10.23 3.478 19.597 10.196L778.63 432.5q12.902 9.531 20.245 23.941 7.343 14.41 7.343 30.516v386.891q0 28.104-20.084 48.117-20.084 20.013-48.286 20.013H561.435V654.565h-162.87v287.413H222.152Z"/></svg>
+              <p class="h6">Inicio</p>
+          </div>
+          </a>
+            <a href="" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          <div class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg"  fill="#E0DC43" height="35" viewBox="0 96 960 960" width="35"><path d="M182.152 982.696q-27.599 0-47.865-21.766-20.265-21.766-20.265-48.365V422.979q-14.24-2.665-27.12-21.231-12.88-18.566-12.88-42.096v-121.5q0-26.697 20.265-47.533 20.266-20.837 47.865-20.837h675.696q26.697 0 47.533 20.837 20.837 20.836 20.837 47.533v121.5q0 23.53-13 42.096-13 18.566-27 21.231v489.586q0 26.599-20.837 48.365-20.836 21.766-47.533 21.766H182.152Zm635.696-623.044v-121.5H142.152v121.5h675.696Zm-429.761 275.5h183.922q13.319 0 22.329-9.02 9.01-9.021 9.01-22.355 0-13.538-9.01-22.397-9.01-8.858-22.329-8.858H388.087q-13.462 0-22.329 8.986t-8.867 22.504q0 13.417 8.867 22.279 8.867 8.861 22.329 8.861Z"/></svg>
+            <p  id="proveedores">Proveedores</p>
+          </div>
+          </a>
+         <a href="" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">
+          <div class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg"   fill="#E09C43" height="35" viewBox="0 96 960 960" width="35"><path d="M286.788 977.63Q257 977.63 236 956.418q-21-21.212-21-51t21.004-50.788q21.005-21 50.5-21 29.496 0 50.996 21.213 21.5 21.212 21.5 51 0 29.787-21.212 50.787-21.213 21-51 21Zm400 0Q657 977.63 636 956.418q-21-21.212-21-51t21.004-50.788q21.005-21 50.5-21 29.496 0 50.996 21.213 21.5 21.212 21.5 51 0 29.787-21.212 50.787-21.213 21-51 21ZM277 769.63q-43.913 0-63.13-29.434-19.218-29.435.739-65.631L277.891 558 127.326 238.63H80q-13.196 0-22.413-9.672-9.218-9.673-9.218-23.341 0-13.667 9.382-22.957 9.381-9.29 23.249-9.29h67.043q10.196 0 18.52 5.006 8.324 5.007 12.067 14.146l28.044 59.848h582.065q37.261 0 46.478 21.652 9.218 21.652-6.456 49.478L697.826 559.565q-10.478 17.718-28.922 30.576Q650.46 603 628.401 603H327.109l-54.326 101.37H730q13.196 0 22.413 9.672 9.217 9.673 9.217 23.341 0 13.667-9.381 22.957-9.381 9.29-23.249 9.29H277Z"/></svg>
+            <p class="h6">Productos</p>
+          </div>
+          </a>
+           <a href="" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop5" >
+          <div class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#88E04F" height="35" viewBox="0 96 960 960" width="35"><path d="M31.196 818.87q-14.424 0-24.245-9.821-9.82-9.821-9.82-24.245V763q0-40.766 42.086-65.318Q81.304 673.13 150 673.13q10.428 0 20.712.631 10.284.63 19.788 1.652-8.239 17.717-12.598 36.523-4.359 18.806-4.359 40.064v66.87H31.196Zm240 0q-14.424 0-24.245-9.821-9.821-9.821-9.821-24.245V752q0-66.464 67.183-107.667 67.182-41.203 175.654-41.203 109.277 0 176.09 41.203T722.87 752v32.804q0 14.424-9.821 24.245-9.821 9.821-24.245 9.821H271.196Zm515.261 0v-66.976q0-21.073-4.239-40.038-4.24-18.965-12.718-36.716 9.504-1.249 19.788-1.629 10.284-.381 20.794-.381 68.114 0 110.451 24.552Q962.87 722.234 962.87 763v21.804q0 14.424-9.821 24.245-9.821 9.821-24.245 9.821H786.457ZM149.941 643.848q-29.898 0-50.756-21.046-20.859-21.046-20.859-50.956 0-29.629 20.988-50.607 20.987-20.978 50.815-20.978t50.806 21.067q20.978 21.066 20.978 50.815 0 29.748-21.037 50.727-21.037 20.978-50.935 20.978Zm660 0q-29.898 0-50.756-21.046-20.859-21.046-20.859-50.956 0-29.629 20.987-50.607 20.988-20.978 50.816-20.978 29.828 0 50.806 21.067 20.978 21.066 20.978 50.815 0 29.748-21.037 50.727-21.037 20.978-50.935 20.978ZM480 573.37q-51.196 0-87.033-35.837T357.13 450.5q0-51.196 35.837-87.033T480 327.63q51.196 0 87.033 35.837T602.87 450.5q0 51.196-35.837 87.033T480 573.37Z"/></svg>
+            <p class="h6">Clientes</p>
+          </div>
+          </a>
+          <a href="" class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop3">
+          <div class="icon">  
+            <svg xmlns="http://www.w3.org/2000/svg"  fill="#DC70E0" height="35" viewBox="0 96 960 960" width="35"><path d="M312.152 774.326h210.37q12.75 0 21.375-8.675 8.625-8.676 8.625-21.5 0-12.825-8.625-21.325t-21.375-8.5h-210.37q-12.75 0-21.375 8.675-8.625 8.676-8.625 21.5 0 12.825 8.625 21.325t21.375 8.5Zm0-167.848h335.696q12.75 0 21.375-8.675 8.625-8.676 8.625-21.5 0-12.825-8.625-21.325t-21.375-8.5H312.152q-12.75 0-21.375 8.676-8.625 8.675-8.625 21.5 0 12.824 8.625 21.324 8.625 8.5 21.375 8.5Zm0-167.848h335.696q12.75 0 21.375-8.675t8.625-21.5q0-12.825-8.625-21.325t-21.375-8.5H312.152q-12.75 0-21.375 8.676-8.625 8.675-8.625 21.5 0 12.824 8.625 21.324 8.625 8.5 21.375 8.5Zm-130 504.066q-28.104 0-48.117-20.084-20.013-20.083-20.013-48.286V278.63q0-28.103 20.013-48.117 20.013-20.013 48.117-20.013h199.022q6.435-35.239 34.152-57.62Q443.043 130.5 480 130.5q36.957 0 64.674 22.38 27.717 22.381 34.152 57.62h199.022q28.202 0 48.286 20.013 20.084 20.014 20.084 48.117v595.696q0 28.203-20.084 48.286-20.084 20.084-48.286 20.084H182.152ZM480 260.913q14.829 0 25.632-10.803t10.803-25.512q0-14.71-10.803-25.632-10.803-10.923-25.632-10.923t-25.632 10.907q-10.803 10.907-10.803 25.528 0 14.829 10.803 25.632T480 260.913Z"/></svg> 
+            <p class="h6">Ventas</p>
+          </div>
+          </a>
+          
+<!-- Modal Proveedores-->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Proveedores</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+     
+      <?php include("../proveedores_attempt/path4.php") ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--____________________________________________________________________________________________________________________-->
+
+<div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel2" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel2">Productos</h1>     
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+     
+      <?php include("../php/path2.php") ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--____________________________________________________________________________________________________________________-->
+
+<div class="modal fade" id="staticBackdrop3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel3" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel3">Ventas</h1>     
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+     
+      <?php include("../php/path3.php") ?>
+      </div>
+    </div>
+  </div>
+</div>
+<!--_____________________________________________________________________________________________________________________-->
+
+<div class="modal fade" id="staticBackdrop5" data-bs-backdrop="static" data-bs-keyboard="false"
+ tabindex="-1" aria-labelledby="staticBackdropLabel5" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel5">Clientes</h1>     
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+     
+      <?php include("../Clientes_attempt/path5.php") ?>
+      </div>
+    </div>
+  </div>
+</div>
+    </body>
+</html>
